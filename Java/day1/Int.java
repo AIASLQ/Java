3865db909ba65bcc29f30234fd17b46d1c568266
AIASLQ <257735753@qq.com>
2018-03-18 21:44:36 +0800

    提交说明

---
+++ Java/day1/Int.java
@@ -0,0 +1,12 @@
+class Int
+{ 
+     public static void main(String[] args)
+    {
+		 int age = 18;
+		 for (int i = 0; i<20; i++){
+			age += i;
+			System.out.println(age);
+		 }
+         System.out.println(age);
+    }
+}
---
+++ Java/day1/expression.java
@@ -0,0 +1,8 @@
+//表达式
+public class expression
+{  
+     public static void main(String[] args)
+    {
+		 //和数学差不多
+    }
+}
